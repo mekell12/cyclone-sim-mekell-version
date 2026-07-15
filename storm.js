5d5000f94c4864a9842ac113c11a8245ab8ce544
mekell12 <seeders_rats_1w@icloud.com>
2026-07-15 08:04:08 -0700

    Forgot to wrap organization in a variable

--- storm.js
+++ storm.js
@@ -213,6 +213,7 @@ class Storm{
             let st = advC.windSpeed;
             let lwc = advC.lowerWarmCore;
             let uwc = advC.upperWarmCore;
+            let og = advC.organization;
             let pos = advC.pos;
             let sb = land.getSubBasin(advX.coord());
             let scale = basin.getScale(sb);
@@ -268,7 +269,7 @@ class Storm{
             stormIcons.fill(0);
             if(simSettings.showStrength){
                 stormIcons.textSize(10);
-                stormIcons.text(simSettings.showDebugProps ? `${displayWindspeed(floor(st), 1)} / ${floor(pr)} hPa\n${round(lwc*1000)/1000} LWC / ${round(uwc*1000)/1000} UWC / ${round(organization*1000)/1000} O` : `${displayWindspeed(floor(st), 1)}\n${floor(pr)} hPa`, 0, DIAMETER + 5);
+                stormIcons.text(simSettings.showDebugProps ? `${displayWindspeed(floor(st), 1)} / ${floor(pr)} hPa\n${round(lwc*1000)/1000} LWC / ${round(uwc*1000)/1000} UWC / ${round(og*1000)/1000} O` : `${displayWindspeed(floor(st), 1)}\n${floor(pr)} hPa`, 0, DIAMETER + 5);
             }
             if(name){
                 stormIcons.textAlign(LEFT,CENTER);
